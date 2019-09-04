10779ddcaf6c9ccc345f6644a81a1d1d699b24d7
Ming Hao Chan <cmh114933@gmail.com>
2019-09-04 22:32:35 +0800

    updated

--- src/routes.ts
+++ src/routes.ts
@@ -5,19 +5,4 @@ export const Routes = [{
     route: "/hello",
     controller: TestController,
     action: "hello"
-},{
-    method: "post",
-    route: "/users",
-    controller: TestController,
-    action: "addUser"
-},{
-    method: "get",
-    route: "/users/:userId",
-    controller: TestController,
-    action: "getUser"
-},{
-    method: "get",
-    route: "/users",
-    controller: TestController,
-    action: "findUsers"
 }];
